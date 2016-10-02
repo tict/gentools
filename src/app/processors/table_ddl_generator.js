96affd57e1a9d2d953b0ff7c8f6b451b453edb08
tomer <takai@takaiict.com>
2016-10-02 12:05:41 +0900

    java parser を組み込んだ。

--- src/app/processors/table_ddl_generator.js
+++ src/app/processors/table_ddl_generator.js
@@ -7,8 +7,20 @@
 
     console.log();
     $.extend(TableDDLGenerator.prototype, modules.processors.AbsProcessor.prototype, {
+        template: {
+            headerComment: '-- DROP TABLE {0}',
+            head: 'CREATE TABLE {0}',
+            annotation: '@CSVColumn(columnIndex={0}, columnName="{1}")',
+            propertyDefinition: 'public String {2};'
+        },
+
         doProcess: function (opt) {
-            return 'asdf';
+            try {
+                var src = JavaParser.parse(opt.data);
+                return JSON.stringify(src);
+            } catch (err) {
+                throw err;
+            }
         }
     });
 
